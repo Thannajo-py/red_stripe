a961188819b25a85d6796caff1a9fe8a89cb38de
Thannajo-py <wolgan_4@hotmail.com>
2021-05-10 14:12:04 +0200

    fix font-family bug remove bootstrap for own css

--- fil_rouge_prod/ludo/ludorecherche/forms.py
+++ fil_rouge_prod/ludo/ludorecherche/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 
 
-from.models import Language, PlayingMode, Difficulty, Tag
+from.models import Language, PlayingMode, Difficulty, Tag, Mechanism, Topic
 
 
 LANGUAGES_CHOICES = [(language.name, language.name) for language in Language.objects.all()]
 PLAYING_MODE_CHOICES = [(playing_mode.name, playing_mode.name) for playing_mode in PlayingMode.objects.all()]
 DIFFICULTY_CHOICES = [(difficulty.name, difficulty.name) for difficulty in Difficulty.objects.all()]
 TAG_CHOICES = [(tag.name, tag.name) for tag in Tag.objects.all()]
+MECHANISM_CHOICES = [(mechanism.name, mechanism.name) for mechanism in Mechanism.objects.all()]
+TOPIC_CHOICES = [(topic.name, topic.name) for topic in Topic.objects.all()]
 
 
 class SearchAdvForm(forms.Form):
@@ -26,3 +28,7 @@ class SearchAdvForm(forms.Form):
                                          choices=LANGUAGES_CHOICES)
     playing_mode_choice = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'select'}),
                                                     choices=PLAYING_MODE_CHOICES)
+    topic = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'select'}),
+                                                    choices=TOPIC_CHOICES)
+    mechanism = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'select'}),
+                                                    choices=MECHANISM_CHOICES)
